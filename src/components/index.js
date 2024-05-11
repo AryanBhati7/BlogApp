00476e5a50ad04578b615dd1f55aaf09c72b8e24
Aryan Bhati <aryanbhati9638@gmail.com>
2024-05-11 19:48:45 +0530

    Post - Form add

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
 import { LogoutBtn } from "./Header/LogoutBtn";
-import { LoginBtn } from "./Header/LoginBtn";
+
 import { Logo } from "./Logo";
 import { Button } from "./Button";
 import { Input } from "./Input";
+import { Select } from "./Select";
+import { RTE } from ".RTE";
 
-export { Header, Footer, Logo, LogoutBtn, LoginBtn, Button, Input };
+export { Header, Footer, Logo, LogoutBtn, Select, Button, RTE, Input };
